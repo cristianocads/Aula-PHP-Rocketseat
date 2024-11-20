fdc312dadb132f8176f347200a582aa9342e78d9
Cristiano Aguiar <cristianocads@gmail.com>
2024-11-20 11:52:48 -0300

    hello world index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Aula PHP da Rocketseat";
+            
+                
+        ?>
+        <p>
+            <?php
+            echo "teste de parágrafo";
+            ?>
+
+        </p>
+
+
+
+
+    </h1>
+    
+</body>
+</html>
